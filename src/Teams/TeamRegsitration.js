e7846eaf800691eb62c5fd495f3569a1d60a456d
Raheedm <raheedmuzawar@gmail.com>
2024-02-21 03:49:52 +0530

    minor

--- src/Teams/TeamRegsitration.js
+++ src/Teams/TeamRegsitration.js
@@ -224,7 +224,7 @@ function TeamRegistration() {
       formData.append('idCardImage', idCardImage);
 
       try {
-        const response = await fetch('http://localhost:5000/api/teamregfile', {
+        const response = await fetch('https://i-rix.tech/api/teamregfile', {
           method: 'POST',
           body: formData,
         });
